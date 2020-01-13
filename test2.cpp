abfbd4a6c51cb050d45d3502a7f1ce9457463722
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-01-13 11:44:01 -0500

    Create test2.cpp

---
+++ test2.cpp
@@ -0,0 +1,63 @@
+// Tweaked example from standard example/test.cpp
+#include <stdint.h>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include "headers/MLX90640_API.h"
+
+#define FMT_STRING "%+06.2f,"
+
+#define MLX_I2C_ADDR 0x33
+
+int main(){
+    int state = 0;
+    static uint16_t eeMLX90640[832];
+    float emissivity = 1;
+    uint16_t frame[834];
+    static float image[768];
+    float eTa;
+    static uint16_t data[768*sizeof(float)];
+
+    std::fstream fs;
+
+    MLX90640_SetDeviceMode(MLX_I2C_ADDR, 0);
+    MLX90640_SetSubPageRepeat(MLX_I2C_ADDR, 0);
+    MLX90640_SetRefreshRate(MLX_I2C_ADDR, 0b010);
+    MLX90640_SetChessMode(MLX_I2C_ADDR);
+
+    paramsMLX90640 mlx90640;
+    MLX90640_DumpEE(MLX_I2C_ADDR, eeMLX90640);
+    MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
+
+    int refresh = MLX90640_GetRefreshRate(MLX_I2C_ADDR);
+
+    int frames = 30;
+    int subpage;
+    static float mlx90640To[768];
+        state = !state;
+        MLX90640_GetFrameData(MLX_I2C_ADDR, frame);
+        eTa = MLX90640_GetTa(frame, &mlx90640);
+        subpage = MLX90640_GetSubPageNumber(frame);
+        MLX90640_CalculateTo(frame, &mlx90640, emissivity, eTa, mlx90640To);
+
+        MLX90640_BadPixelsCorrection((&mlx90640)->brokenPixels, mlx90640To, 1, &mlx90640);
+        MLX90640_BadPixelsCorrection((&mlx90640)->outlierPixels, mlx90640To, 1, &mlx90640);
+
+        for(int z = 0; z < 24; z++) {
+           std::cout << "temp" << z;
+           if(z < 23) std::cout << ",";
+        }
+        std::cout << std::endl;
+        for(int x = 0; x < 32; x++){
+            for(int y = 0; y < 24; y++){
+                float val = mlx90640To[32 * (23-y) + x];
+                if(val < 0) val = 0;
+                std::cout << val;
+                if(y<23) std::cout << ",";
+            }
+            std::cout << std::endl;
+        }
+    return 0;
+}
